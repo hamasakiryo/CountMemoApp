61b73c921ab6e05b04b5c2b40e283a4e64ca6a10
hamasakiryo <hamaan.0927@icloud.com>
2023-11-21 06:06:09 +0900

    新しいファイルとしてメモを編集できるViewを作成

---
+++ CountMemoApp/EditCountMemoView.swift
@@ -0,0 +1,31 @@
+//
+//  EditCountMemoView.swift
+//  CountMemoApp
+//
+//  Created by 浜崎良 on 2023/11/21.
+//
+
+import SwiftUI
+
+struct EditCountMemoView: View {
+    @State var MemoTitleText: String
+    @State var MemoContentText: String
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                TextField("タイトルを入力", text: $MemoTitleText)
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .padding(.leading, 10.0)
+                Divider()
+                TextEditor(text: $MemoContentText)
+                    .padding(.horizontal, 10.0)
+            }
+        }
+    }
+}
+
+#Preview {
+    EditCountMemoView(MemoTitleText: "タイトル1", MemoContentText: "内容1")
+}
