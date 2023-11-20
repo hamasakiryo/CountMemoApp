9b5e8cc0eec218f09f2783c9e7ef50a04f74228c
hamasakiryo <hamaan.0927@icloud.com>
2023-11-20 16:39:27 +0900

    Initial Commit

---
+++ CountMemoApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CountMemoApp
+//
+//  Created by 浜崎良 on 2023/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CountMemoApp/CountMemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountMemoAppApp.swift
+//  CountMemoApp
+//
+//  Created by 浜崎良 on 2023/11/20.
+//
+
+import SwiftUI
+
+@main
+struct CountMemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
