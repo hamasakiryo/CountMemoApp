5f9ef2501b73a7d121f74ac0c177c224ddbd94c0
hamasakiryo <hamaan.0927@icloud.com>
2023-11-29 00:07:58 +0900

    追加

--- CountMemoApp/CountMemo.swift
+++ CountMemoApp/CountMemo.swift
@@ -14,10 +14,3 @@ struct CountMemo: Identifiable {
     var date: String
     var characterCount: Int
 }
-
-class CountMemoData: ObservableObject {
-    @Published var memos: [CountMemo] = [
-        CountMemo(title: "タイトル1", content: "内容1", date: "2023\n11/21", characterCount: 1000),
-        CountMemo(title: "タイトル2", content: "内容2", date: "2023\n11/22", characterCount: 2000)
-    ]
-}
