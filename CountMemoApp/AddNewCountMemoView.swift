e154950c97914602996c3284c64533c2564072de
hamasakiryo <hamaan.0927@icloud.com>
2023-12-14 01:58:43 +0900

    refactor: onChangeでnewMemoContentTextの変更を監視して文字数を更新するように変更

--- CountMemoApp/AddNewCountMemoView.swift
+++ CountMemoApp/AddNewCountMemoView.swift
@@ -13,6 +13,7 @@ struct AddNewCountMemoView: View {
     @State var newMemoTitleText = ""
     @State var newMemoContentText = ""
     @State var characterLimit = ""
+    @State var characterCount = 0
     @State var isShowCountSettingView = false
     @State var includeSpace = false
     @State var includeNewLine = false
@@ -20,59 +21,66 @@ struct AddNewCountMemoView: View {
     @State var switchCountdown = false
     
     var body: some View {
-        let characterCount = memoData.modifiedTextCharacterCount(text: newMemoContentText, characterLimit: characterLimit,includeSpace: includeSpace, includeNewLine: includeNewLine, removeEnclosedText: removeEnclosedText, switchCountdown: switchCountdown)
-        
         NavigationStack {
             VStack {
                 Text("\(switchCountdown ? "残:" : "計:")\(characterCount)")
-                .foregroundStyle(characterCount < 0 ? .red : .primary)
-                .font(.title)
-                .fontWeight(.bold)
-            TextField("タイトルを入力", text: $newMemoTitleText)
-                .font(.title)
-                .fontWeight(.bold)
-                .padding(.leading, 10.0)
-            Divider()
-            TextEditor(text: $newMemoContentText)
-                .padding(.horizontal, 10.0)
-        }
-        .sheet(isPresented: $isShowCountSettingView) {
-            CountSettingView(includeSpace: $includeSpace, includeNewLine: $includeNewLine, removeEnclosedText: $removeEnclosedText, switchCountDown: $switchCountdown, charcterLimit: $characterLimit)
-                .presentationDetents([.medium])
-        }
-        .navigationBarTitleDisplayMode(.inline)
-        .navigationBarBackButtonHidden(true)
-        .toolbar {
-            
-            ToolbarItem(placement: .topBarTrailing) {
-                Button{
-                    isShowCountSettingView = true
-                } label: {
-                    Image(systemName: "gearshape.fill")
-                        .foregroundStyle(Color.primary)
-                        .font(.title)
+                    .foregroundStyle(characterCount < 0 ? .red : .primary)
+                    .font(.title)
+                    .fontWeight(.bold)
+                TextField("タイトルを入力", text: $newMemoTitleText)
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .padding(.leading, 10.0)
+                Divider()
+                TextEditor(text: $newMemoContentText)
+                    .onChange(of: newMemoContentText) {
+                        characterCount = memoData.modifiedTextCharacterCount(
+                            text: newMemoContentText,
+                            characterLimit: characterLimit,
+                            includeSpace: includeSpace,
+                            includeNewLine: includeNewLine,
+                            removeEnclosedText: removeEnclosedText,
+                            switchCountdown: switchCountdown)
+                    }
+                    .padding(.horizontal, 10.0)
+            }
+            .sheet(isPresented: $isShowCountSettingView) {
+                CountSettingView(includeSpace: $includeSpace, includeNewLine: $includeNewLine, removeEnclosedText: $removeEnclosedText, switchCountDown: $switchCountdown, charcterLimit: $characterLimit)
+                    .presentationDetents([.medium])
+            }
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarBackButtonHidden(true)
+            .toolbar {
+                
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button{
+                        isShowCountSettingView = true
+                    } label: {
+                        Image(systemName: "gearshape.fill")
+                            .foregroundStyle(Color.primary)
+                            .font(.title)
+                        
+                    }
                 }
             }
-        }
-    }
-                .toolbar {
-                    ToolbarItem(placement: .topBarLeading) {
-                        Button("<リスト") {
-                            memoData.addNewMemo(newMemoTitleText: newMemoTitleText,
-                                                newMemoContentText: newMemoContentText, characterLimit: characterLimit,
-                                                characterCount: characterCount,
-                                                includeSpace: includeSpace,
-                                                includeNewLine: includeNewLine,
-                                                removeEnclosedText: removeEnclosedText,
-                                                switchCountdown: switchCountdown
-                                                )
-                            dismiss()
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button("<リスト") {
+                        memoData.addNewMemo(newMemoTitleText: newMemoTitleText,
+                                            newMemoContentText: newMemoContentText, characterLimit: characterLimit,
+                                            characterCount: characterCount,
+                                            includeSpace: includeSpace,
+                                            includeNewLine: includeNewLine,
+                                            removeEnclosedText: removeEnclosedText,
+                                            switchCountdown: switchCountdown
+                        )
+                        dismiss()
                     }
                 }
             }
         }
     }
-
+}
 
 #Preview {
     AddNewCountMemoView(memoData: CountMemoManager())
