b33994a489093ba474f91271a7d04198c22e7077
hamasakiryo <hamaan.0927@icloud.com>
2023-12-18 19:17:06 +0900

    refactor: CountMemoListRowViewはmemoプロパティを表示するだけなので定数に変更

--- CountMemoApp/CountMemoListRowView.swift
+++ CountMemoApp/CountMemoListRowView.swift
@@ -8,26 +8,22 @@
 import SwiftUI
 
 struct CountMemoListRowView: View {
-    var memo: CountMemo
+    let memo: CountMemo
     
     var body: some View {
         VStack {
-            HStack{
                 Text(memo.title)
                     .lineLimit(1)
                     .font(.title2)
                     .fontWeight(.bold)
                     .frame(maxWidth: .infinity, alignment: .leading)
-            }
             HStack {
                 Text(memo.date.formatted())
                     .padding(.trailing, 10.0)
-                HStack{
                     Text(memo.content)
                         .lineLimit(2)
                         .foregroundStyle(.gray)
                         .frame(maxWidth: .infinity, alignment: .leading)
-                }
                 RoundedRectangle(cornerRadius: 20)
                     .foregroundStyle(.orisinal)
                     .frame(width: 70, height: 30)
