87d130027ca50d1f7726a5c33fb5b9df0605ea28
hamasakiryo <hamaan.0927@icloud.com>
2023-12-23 20:49:44 +0900

    refactor: CountMemoListRowViewをCountMemoListRowに名称変更

--- CountMemoApp/CountMemoListRow.swift
+++ CountMemoApp/CountMemoListRow.swift
@@ -7,23 +7,23 @@
 
 import SwiftUI
 
-struct CountMemoListRowView: View {
+struct CountMemoListRow: View {
     let memo: CountMemo
     
     var body: some View {
         VStack {
-                Text(memo.title)
-                    .lineLimit(1)
-                    .font(.title2)
-                    .fontWeight(.bold)
-                    .frame(maxWidth: .infinity, alignment: .leading)
+            Text(memo.title)
+                .lineLimit(1)
+                .font(.title2)
+                .fontWeight(.bold)
+                .frame(maxWidth: .infinity, alignment: .leading)
             HStack {
                 Text(memo.date.formatted())
                     .padding(.trailing, 10.0)
-                    Text(memo.content)
-                        .lineLimit(2)
-                        .foregroundStyle(.gray)
-                        .frame(maxWidth: .infinity, alignment: .leading)
+                Text(memo.content)
+                    .lineLimit(2)
+                    .foregroundStyle(.gray)
+                    .frame(maxWidth: .infinity, alignment: .leading)
                 RoundedRectangle(cornerRadius: 20)
                     .foregroundStyle(.orisinal)
                     .frame(width: 70, height: 30)
@@ -34,5 +34,5 @@ struct CountMemoListRowView: View {
 }
 
 #Preview {
-    CountMemoListRowView(memo: CountMemo())
+    CountMemoListRow(memo: CountMemo())
 }
