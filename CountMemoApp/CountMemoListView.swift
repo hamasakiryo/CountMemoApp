0fdc22b1b3315ea614d9b83c395bc5fb47cab445
hamasakiryo <hamaan.0927@icloud.com>
2023-12-24 00:22:28 +0900

    refactor: ModifiedTextCharacterCountを切り出す

--- CountMemoApp/CountMemoListView.swift
+++ CountMemoApp/CountMemoListView.swift
@@ -9,13 +9,13 @@ import SwiftUI
 import RealmSwift
 
 struct CountMemoListView: View {
-    @ObservedResults(CountMemo.self, configuration: Realm.Configuration(schemaVersion: 2)) var memos
+    @ObservedResults(CountMemo.self, configuration: Realm.Configuration(schemaVersion: 3)) var memos
     
     var body: some View {
         NavigationStack {
             List(memos) { memo in
                 NavigationLink(destination: EditCountMemoView(memo: memo)) {
-                    CountMemoListRowView(memo: memo)
+                    CountMemoListRow(memo: memo)
                         .swipeActions(edge: .trailing) {
                             Button{$memos.remove(memo)} label: {
                                 Image(systemName: "trash")
@@ -24,10 +24,19 @@ struct CountMemoListView: View {
                         }
                 }
             }
-            .navigationTitle("リスト")
-            .onAppear{
-                //空のメモを削除する処理
+            .onAppear {
+                // Realmインスタンスを取得
+                let realm = try! Realm(configuration: Realm.Configuration(schemaVersion: 3))
+                
+                // titleとcontentが両方空のメモをフィルタリング
+                let emptyMemos = realm.objects(CountMemo.self).filter("title == '' AND content == ''")
+                
+                // トランザクション内で空のメモを削除
+                try! realm.write {
+                    realm.delete(emptyMemos)
+                }
             }
+            .navigationTitle("リスト")
             .toolbar{
                 ToolbarItem(placement: .bottomBar) {
                     NavigationLink(destination: AddNewCountMemoView().environment(\.realm, try! .init(configuration: Realm.Configuration()))) {
--- CountMemoApp/EditCountMemoView.swift
+++ CountMemoApp/EditCountMemoView.swift
@@ -12,6 +12,7 @@ struct EditCountMemoView: View {
     @Environment (\.dismiss) private var dismiss
     @ObservedRealmObject var memo: CountMemo
     @State private var isShowCountSettingView = false
+    @State private var textCounter = TextCounter()
     
     var body: some View {
         NavigationStack {
@@ -29,7 +30,13 @@ struct EditCountMemoView: View {
                     .onChange(of: memo.content) {
                         let thawMemo = memo.thaw()
                         try! thawMemo?.realm?.write{
-                            thawMemo?.characterCount = modifiedTextCharacterCount(text: memo.content, characterLimit: memo.characterLimit,includeSpace: memo.includeSpace, includeNewLine: memo.includeNewLine, removeEnclosedText: memo.removeEnclosedText, switchCountdown: memo.switchCountdown)
+                            thawMemo?.characterCount = textCounter.modifiedTextCharacterCount(
+                                text: memo.content,
+                                characterLimit: memo.characterLimit,
+                                includeSpace: memo.includeSpace,
+                                includeNewLine: memo.includeNewLine,
+                                removeEnclosedText: memo.removeEnclosedText,
+                                switchCountdown: memo.switchCountdown)
                         }
                     }
                     .padding(.horizontal, 10.0)
@@ -38,8 +45,8 @@ struct EditCountMemoView: View {
                 CountSettingView(includeSpace: $memo.includeSpace,
                                  includeNewLine: $memo.includeNewLine,
                                  removeEnclosedText: $memo.removeEnclosedText,
-                                 switchCountDown: $memo.switchCountdown, charcterLimit: $memo.characterLimit
-                )
+                                 switchCountDown: $memo.switchCountdown,
+                                 charcterLimit: $memo.characterLimit)
                 .presentationDetents([.medium])
             }
             .navigationBarTitleDisplayMode(.inline)
@@ -64,31 +71,6 @@ struct EditCountMemoView: View {
                 }
             }
         }
-    }
-    ///条件に応じてtextを修正し、その文字数を返すメソッド(EditCountMemoView,AddNewCountMemoViewで使用)
-    func modifiedTextCharacterCount(text: String, characterLimit: String, includeSpace: Bool, includeNewLine: Bool, removeEnclosedText: Bool, switchCountdown: Bool) -> Int {
-        var modifiedText = text
-        
-        //includeSpaceがfalseの場合、テキストから空白を除去(デフォルトで文字数のカウントには空白が含まれない)
-        if !includeSpace {
-            modifiedText = modifiedText.replacingOccurrences(of: "[ 　]", with: "", options: .regularExpression)
-        }
-        //includeNewLineがfalseの場合、テキストから改行を除去(デフォルトで文字数のカウントには改行が含まれない)
-        if !includeNewLine {
-            modifiedText = modifiedText.replacingOccurrences(of: "\n", with: "")
-        }
-        
-        //removeEnclosedTextがtrueの場合、テキストから'//'で囲まれた文字を除去(例: //文字// とすると「文字」が除去される)
-        if removeEnclosedText {
-            modifiedText = modifiedText.replacingOccurrences(of: "//(.*?)//", with: "", options: .regularExpression)
-        }
-        
-        //カウントダウン方式に変更
-        if switchCountdown {
-            return((Int(characterLimit) ?? 0) - modifiedText.count)
-        }
-        
-        return modifiedText.count
     }
 }
 #Preview {
